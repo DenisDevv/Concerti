d6293ff833f2ac7df90f7afb846768627d743698
DenisDevv <karajedenis@gmail.com>
2025-02-03 12:16:40 +0100

    Fixato messaggio di log (Internazionale, Nazionale)

--- src/main/java/live/denisdev/concerti/Logger.java
+++ src/main/java/live/denisdev/concerti/Logger.java
@@ -6,6 +6,8 @@ public class Logger {
         String coloreBlu = "\u001B[34m";
         String coloreBianco = "\u001B[0m";
         String coloreRosso = "\u001B[31m";
+        message = message.replace("true", "Internazionale");
+        message = message.replace("false", "Nazionale");
         if (errore) {
             System.out.println(time + " - " + coloreRosso + message + coloreBianco);
         } else {
